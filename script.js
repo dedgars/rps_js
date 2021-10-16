6291bea58aa77996e9eb27de9a0c2e8065653528
Edgars <duminsh@gmail.com>
2021-10-16 15:37:54 +0300

    somewhat working

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("this is it")
+
+function computerPlay() {
+    const items = ["rock", "paper", "scissors"];
+    randInt = Math.floor(Math.random() * items.length);
+    let itemToPlay = items[randInt];
+    console.log('computer plays ' + itemToPlay)
+    return randInt;
+}
+
+
+function playRound(playerSelection, computerSelection){
+    if ((playerSelection + 1) % 3 == computerSelection) {
+        return('Computer wins')
+    } else if ( playerSelection == computerSelection) {
+        return('Draw')
+    } else {
+        return('Player wins')
+    }
+}
+
+const playerSelection = 0
+const computerSelection = computerPlay()
+
+console.log(playRound(playerSelection, computerSelection))
+
+
